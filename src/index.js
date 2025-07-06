aa1808a38584eadb87fb26c8c90953aac40b0787
IzzieIM <anushanikam05@kgpian.iitkgp.ac.in>
2025-07-06 23:41:31 +0530

    some database connection

---
+++ src/index.js
@@ -0,0 +1,42 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env"
+});
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+app = express();
+
+
+// this is howw we connect to the database
+// we use mongoose to connect to the database
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+    // what is database cannot be connected 
+    //.on is a app listener there are many listeners like this
+    app.on('error', (error) => {
+      console.error('Database connection error:', error);
+      throw err;
+    });
+
+    app.listen(process.env.PORT , () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+}) ()
+*/
